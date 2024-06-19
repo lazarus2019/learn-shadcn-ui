7a7fef3ad5d8e1d2191fc5388a2a3c68384ce421
lazarus2019 <thaisonbk2020@gmail.com>
2024-06-19 15:17:04 +0700

    feat: implement shadcn-ui with button, mode toggle & calendar

---
+++ src/components/ui/mode-toggle.tsx
@@ -0,0 +1,35 @@
+import { Moon, Sun } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useTheme } from '@/providers/theme-provider';
+import { listTheme } from '@/configs/common';
+import { toTitleCase } from '@/helpers/string';
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {listTheme.map((theme) => (
+          <DropdownMenuItem onClick={() => setTheme(theme)} key={theme}>
+            {toTitleCase(theme)}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
---
+++ src/configs/common.ts
@@ -0,0 +1,10 @@
+import { Theme } from '@/types/shadcn';
+
+export const listTheme: Theme[] = [
+  'dark',
+  'light',
+  'system',
+  'green',
+  'orange',
+  'violet',
+];
